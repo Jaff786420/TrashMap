48b99b7b67e4e3598c3d71093076cc512eadb816
Jaff786420 <43278603+Jaff786420@users.noreply.github.com>
2019-03-24 01:34:14 +0530

    Update server2.py

--- server2.py
+++ server2.py
@@ -50,4 +50,4 @@ def removeOne(name):
 
 	mycol.delete_one(myquery) 
 """
-run(reloader=True, debug=True, host='mighty-brook-54547.herokuapp.com', port=8080)
+run(reloader=True, debug=True, host='0.0.0.0', port=8080)
